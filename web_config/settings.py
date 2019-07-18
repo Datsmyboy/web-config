ecc94bfc1a46b655d466363c17abb3e97353b53c
Russel <Russel@example.com>
2019-07-18 15:43:42 +0800

    Updated Settings

--- web_config/settings.py
+++ web_config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +128,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,"web_config", 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, "web_config", 'static', 'static_cdn')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "web_config", "static")
+
+django_heroku.settings(locals())
